fc5394a2032947a72c43f9dd3112e1bc312837ef
Ashish Gupta <ashishkhms@gmail.com>
2025-12-18 23:27:19 +0530

    initial commit

---
+++ src/main/java/com/ashish/threadboost/ThreadboostApplication.java
@@ -0,0 +1,13 @@
+package com.ashish.threadboost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadboostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreadboostApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ashish/threadboost/models/User.java
@@ -0,0 +1,28 @@
+package com.ashish.threadboost.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String userName;
+
+    Integer classEnrolled;
+
+    BigDecimal marks;
+}
